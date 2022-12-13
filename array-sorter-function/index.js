4914e96a59cc4aedfd1698c79aa901cb90096a6d
amaraa666 <amiamaraa66@gmail.com>
2022-12-13 16:26:39 +0800

    hi

--- array-sorter-function/index.js
+++ array-sorter-function/index.js
@@ -1,23 +1,59 @@
 let arr = [4, 32, 2, 5, 8, -10, 20, 1, 0, -20];
-let arr2 =[]; 
+let arr22 = [412345, 3212345, 22345, 534567, 83456, -1044, 2044, 16, 06, -2660];
+
+function myfunc(x){
+    for(let j = 0 ; j < x.length ; j++){
+        for(let i = 0 ; i < x.length ; i++){
+            if(x[i]>x[i+1]){
+                let temp = x[i];
+                x[i] = x[i+1];
+                x[i+1] = temp;
+            }
+        } 
+    } 
+    console.log(x)  
+}
+myfunc(arr);
+myfunc(arr22);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr2 =[]; 
+// // function myFunc(x){
+// //     for(i=0 ; i<x.length ; i++){
+// //         for(j=0 ; j<x.length ; j++){
+// //             if(x[j]>x[j+1]){
+// //                 let temp = x[j];
+// //                 x[j] = x[j+1];
+// //                 x[j+1] = temp;
+// //             }
+// //         }
+// //     }
+// //     console.log(x);
+// // }
+// myFunc(arr);
 // function myFunc(x){
 //     for(i=0 ; i<x.length ; i++){
 //         for(j=0 ; j<x.length ; j++){
-//             if(x[j]>x[j+1]){
-//                 let temp = x[j];
-//                 x[j] = x[j+1];
-//                 x[j+1] = temp;
-//             }
+//             x[0]<x[j+1]
+
 //         }
 //     }
-//     console.log(x);
 // }
-myFunc(arr);
-function myFunc(x){
-    for(i=0 ; i<x.length ; i++){
-        for(j=0 ; j<x.length ; j++){
-            x[0]<x[j+1]
-
-        }
-    }
-}
--- function-even-odd/index.js
+++ function-even-odd/index.js
@@ -1,20 +1,63 @@
-let x = 10;
-let y = 20;
+// 1. 2 тоон утга авдаг 2 function үүсгэ.
+// 2. 2 авсан тоон утгын хоорондох тэгш тоонуудын array буцаадаг 1 function.
+// 2. 2 авсан тоон утгын хоорондох сондгой тоонуудын array буцаадаг 1 function
+let x = Number(prompt("garaas toon utga oruulna uu"));
+let y = Number(prompt("garaas toon oruulna uu"));
 let even = [];
 let odd = [];
 function myFunc(a,b){
     if(a<b){
-        for(let i=a ; i<b ; i++){
-            if(i%2==0){
+        for(let i = a ; i<=b ; i++){
+            if(i%2 == 0){
                 even.push(i);
             }else{
                 odd.push(i);
             }
         }
-    }else if(x>y){
-        console.log("zuv utga oruulna uu?");
+    }else if (b<a){
+        for(let i = b ; i<=a ; i++){
+            if(i%2 == 0){
+                even.push(i);
+            }else{
+                odd.push(i);
+            }
+        }
+    }else{
+        console.log("Adilhan too bainaa dahin oroldno uu?");
     }
+    console.log("tegsh too",even);
+    console.log("sondgoi too",odd);
 }
-myFunc(x,y)
-console.log("tegsh" , even);
-console.log("sondgoi", odd);
+myFunc(x,y);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function myFunc(a,b){
+//     if(a<b){
+//         for(let i=a ; i<b ; i++){
+//             if(i%2==0){
+//                 even.push(i);
+//             }else{
+//                 odd.push(i);
+//             }
+//         }
+//     }else if(x>y){
+//         console.log("zuv utga oruulna uu?");
+//     }
+// }
+// myFunc(x,y)
+// console.log("tegsh" , even);
+// console.log("sondgoi", odd);
